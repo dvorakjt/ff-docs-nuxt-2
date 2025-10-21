5450f491cc20ed3ef702f7a0e8b5ba79d1c2eb86
Joseph Dvorak <dvorakjt@gmail.com>
2025-10-21 01:57:11 -0700

    installed sass

--- app/app.vue
+++ app/app.vue
@@ -5,32 +5,6 @@
   <NuxtRouteAnnouncer />
 </template>
 
-<style>
-* {
-  box-sizing: border-box;
-  font-family: "Roboto Flex";
-  color: white;
-  margin: 0;
-  padding: 0;
-  transform-origin: top left;
-}
-
-html {
-  scrollbar-gutter: stable;
-}
-
-body {
-  background-color: black;
-  margin: 0;
-  padding: 0;
-}
-
-h1,
-h2,
-h3,
-h4,
-h5,
-h6 {
-  font-family: "Monomaniac One";
-}
+<style lang="scss">
+@use '~/assets/scss/main';
 </style>
